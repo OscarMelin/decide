4462c9a59bbea342c5bf9dde72c2a3ef43744f23
Johannes Westlund <johannes.wirkkala.westlund@gmail.com>
2018-01-19 20:11:28 +0100

    feat: preliminary created all necessary function headers

--- src/main/java/AntiMissileSystem.java
+++ src/main/java/AntiMissileSystem.java
@@ -7,7 +7,7 @@ public class AntiMissileSystem {
         ANDD, ORR, NOTUSED
     }
 
-    //------------------ INPUT ------------------------
+    //------------------ Global Data ------------------
 
     // The number of planar data points. 2 <= NUMPOINTS <= 100
     private int numPoints;
@@ -17,9 +17,13 @@ public class AntiMissileSystem {
     // Struct holding parameters for Launch Interceptor Conditions (LIC’s).
     private Parameters parameters;
 
-    // The Logical Connector Matrix (LCM)is a 15x15 symmetric matrix that
+    // The Logical Connector Matrix (LCM) is a 15x15 symmetric matrix that
     // defines which individual LIC’s that must be considered jointly in some way.
     private LcmEntry[][] lcm;
+
+    // The Conditions Met Vector (CMV) is set according to the results of each LIC.
+    private boolean[] cmv;
+
     // The Preliminary Unlocking Vector (PUV) represents which LIC actually matters
     // in this particular launch determination.
     private boolean[] puv;
@@ -44,8 +48,73 @@ public class AntiMissileSystem {
         //---------------------
     }
 
-    
+    /**
+     *
+     * @return whether an interceptor should be launched
+     */
+    //TODO: The actual return value should be a string of either "YES" or "NO"
+    public boolean decide() {
+        return false;
+    }
 
 
+    public boolean lic0() {
+        return false;
+    }
 
+    public boolean lic1() {
+        return false;
+    }
+
+    public boolean lic2() {
+        return false;
+    }
+
+    public boolean lic3() {
+        return false;
+    }
+
+    public boolean lic4() {
+        return false;
+    }
+
+    public boolean lic5() {
+        return false;
+    }
+
+    public boolean lic6() {
+        return false;
+    }
+
+    public boolean lic7() {
+        return false;
+    }
+
+    public boolean lic8() {
+        return false;
+    }
+
+    public boolean lic9() {
+        return false;
+    }
+
+    public boolean lic10() {
+        return false;
+    }
+
+    public boolean lic11() {
+        return false;
+    }
+
+    public boolean lic12() {
+        return false;
+    }
+
+    public boolean lic13() {
+        return false;
+    }
+
+    public boolean lic14() {
+        return false;
+    }
 }
