f2773c2972e11dc2c1f8145de0a48affab670eba
Johannes Westlund <johannes.wirkkala.westlund@gmail.com>
2018-01-21 22:31:48 +0100

    feat: Add test for the lic2 funtionality

--- src/test/java/AntiMissileSystemTest.java
+++ src/test/java/AntiMissileSystemTest.java
@@ -17,4 +17,32 @@ public class AntiMissileSystemTest {
 
         assertFalse(antiMissileSystem.decide());
     }
+
+    @Test
+    void testLic2_1() {
+        // Contract: The function "lic2" returns true if there exists a set of three consecutive
+        // points that form an angle (where the second point is the vertex of the angel)
+        // that is greater than PI+epsilon
+
+        int numPoints = 3;
+        Point[] points = {new Point(-1.0,-1.0), new Point(0.0,0.0), new Point(1.0,0.0)};
+        Parameters parameters = new Parameters();
+        parameters.epsilon = Math.PI/18.0; // Epsilon is 10 degrees (measured in radians)
+        AntiMissileSystem testSystem = new AntiMissileSystem(numPoints, points, parameters, null,null);
+        assert(testSystem.lic2());
+    }
+
+    @Test
+    void testLic2_2() {
+        // Contract: The function "lic2" returns true if there exists a set of three consecutive
+        // points that form an angle (where the second point is the vertex of the angel)
+        // that is less than PI-epsilon
+
+        int numPoints = 3;
+        Point[] points = {new Point(-1.0,-1.0), new Point(0.0,0.0), new Point(1.0,0.0)};
+        Parameters parameters = new Parameters();
+        parameters.epsilon = Math.PI/4.001; // Epsilon is a little bit less than 45 degrees (measured in radians)
+        AntiMissileSystem testSystem = new AntiMissileSystem(numPoints, points, parameters, null,null);
+        assert(testSystem.lic2());
+    }
 }
