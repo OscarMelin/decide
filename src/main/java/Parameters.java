d0345740343198ee9a89e75fe0ceb41dd1fba9ec
Johannes Westlund <johannes.wirkkala.westlund@gmail.com>
2018-01-22 21:44:33 +0100

    fix: change names of two parameters to length1 and radius1

--- src/main/java/Parameters.java
+++ src/main/java/Parameters.java
@@ -1,8 +1,8 @@
 public class Parameters {
 
     // Values used for the different Launch Interceptor Conditions (LIC’s).
-    public double length; // Length in LICs 0, 7, 12
-    public double radius; // Radius in LICs 1, 8, 13
+    public double length1; // Length in LICs 0, 7, 12
+    public double radius1; // Radius in LICs 1, 8, 13
     public double epsilon; // Deviation from PI in LICs 2, 9
     public double area1; // Area in LICs 3, 10, 14
     public int qPts; // No. of consecutive points in LIC 4
