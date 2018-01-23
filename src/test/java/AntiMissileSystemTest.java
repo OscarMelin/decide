60802e72fb4c87e5a4f26b2113b46100727bcef0
Johannes Westlund <johannes.wirkkala.westlund@gmail.com>
2018-01-23 13:43:59 +0100

    feat: Implement lic9 (#30)

--- src/test/java/AntiMissileSystemTest.java
+++ src/test/java/AntiMissileSystemTest.java
@@ -88,6 +88,31 @@ public class AntiMissileSystemTest {
     }
   
     @Test
+    void testLic9() {
+        // Contract: Lic9 returns true iff there exists a set of three consecutive points
+        // (separated by C_PTS and D_PTS points) that form an angle
+        // (where the second point is the vertex of the angel) that is greater than PI+epsilon
+        // or less than PI-epsilon. Lic9 returns false otherwise and if numpoints < 5.
+        int numpoints = 3;
+        Point[] points = {new Point(0.0,0.0), new Point(1.0,1.0), new Point(-1.0,-1.0)};
+        Parameters parameters = new Parameters();
+        AntiMissileSystem testSystem = new AntiMissileSystem(numpoints,points,parameters,null,null);
+        assertFalse(testSystem.lic9());
+
+        testSystem.numPoints+=2;
+        Point[] newPoints = {new Point(-1.0,-1.0), new Point(24.534,232.4),
+                new Point(0.0,0.0), new Point(1.0,0.0), new Point(1.0, -1.0)};
+        testSystem.points = newPoints;
+        testSystem.parameters.cPTS = 1;
+        testSystem.parameters.dPTS = 1;
+        testSystem.parameters.epsilon = PI*0.5; // Epsilon is 90 degrees (measured in radians)
+        assertFalse(testSystem.lic9());
+
+        testSystem.parameters.epsilon = PI*0.499; // Epsilon is almost 90 degrees (measured in radians)
+        assertTrue(testSystem.lic9());
+    }
+  
+    @Test
     void testLic10() {
         // Contract: Lic10 returns true iff there exists at least one set of three data points
         // separated by exactly E_PTS and F_PTS consecutive intervening points, respectively,
