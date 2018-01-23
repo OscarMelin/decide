e1abc9a75d0b9b7e4777a95e68849f859407ef49
Sara Ervik <servik@kth.se>
2018-01-23 22:06:44 +0100

    fix: Remove example test used as guide for manipulating parameters (#54)

--- src/test/java/AntiMissileSystemTest.java
+++ src/test/java/AntiMissileSystemTest.java
@@ -8,21 +8,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class AntiMissileSystemTest {
 
     @Test
-    void test1() {
-        //Example of how you can manipulate the parameters.
-        int numPoints = 1;
-        Point[] points = {new Point(20.0, 30.0)};
-        Parameters parameters = new Parameters();
-        Connector[][] lcm = new Connector[15][15];
-        lcm[0][0] = Connector.NOTUSED;
-        lcm[0][1] = Connector.ANDD;
-        boolean[] puv = new boolean[15];
-        AntiMissileSystem antiMissileSystem = new AntiMissileSystem(numPoints, points, parameters, lcm, puv);
-
-        assertFalse(antiMissileSystem.decide());
-    }
-  
-    @Test
     void testLic0() {
         // contract: lic0 returns true if two consecutive data point are a greater distance than the length length1
         // defined in the parameters, false otherwise
